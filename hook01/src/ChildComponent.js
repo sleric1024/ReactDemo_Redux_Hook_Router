f3370ca67428d6346a2e64a6d803daf3b9f01a69
Eric Lu <sleric1024@gmail.com>
2019-11-14 11:33:33 +0800

    Add useCallback testing

--- hook01/src/ChildComponent.js
+++ hook01/src/ChildComponent.js
@@ -1,16 +1,22 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useCallback} from 'react';
 
 function ChildComponent(props) {
   function changeDog(name) {
-    console.log("CHANGE_CHANGE_CHANGE");
+    console.log(name + "CHANGE_CHANGE_CHANGE");
     return name + ' COMING';
   }
 
-  const actionDog = useMemo(() => changeDog(props.name), [props.name]);
+  //const actionDog = useMemo(() => changeDog(props.name), [props.name]);
+  const actionDog = useCallback(
+    () => {
+      return changeDog(props.name);
+    },
+    [props.name],
+  );
 
   return (
     <div>
-        <h1>{actionDog}</h1>
+        <h1>{actionDog()}</h1>
         <h1>{props.children}</h1>
     </div>
   )
